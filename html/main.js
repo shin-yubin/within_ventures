6b3a42072176232a8b3cc7bf11b623bb1fe39fc7
shin-yubin <75112390+shin-yubin@users.noreply.github.com>
2020-12-19 11:30:33 +0900

    full page js layout arrange

---
+++ html/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  // fullpage customization
+  $('#fullpage').fullpage({
+    sectionSelector: '.section',
+    navigation: false,
+    slidesNavigation: true,
+    css3: true,
+    controlArrows: false,
+  });
+});
